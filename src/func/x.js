e06b092163e45a33e7fc4d50fed77044eb29aae5
Dev Dabke <dev@dabke.com>
2020-05-29 03:00:16 -0400

    Update type annotation for x

--- src/func/x.js
+++ src/func/x.js
@@ -1,13 +1,14 @@
 // @flow
 import ReflexiveDiGraph from '../object/ReflexiveDiGraph';
 import liftingReducer from './liftingReducer';
+import type { Label } from '../object/Vertex';
 
 function x(g: ReflexiveDiGraph, h: ReflexiveDiGraph): ReflexiveDiGraph {
     const o = new ReflexiveDiGraph();
     const outboundVertices = Object.keys(g.vertices);
 
     // TODO: we just assume that all vertices are well-defined in both graphs
-    const newEdgesByVertexLabel = outboundVertices.map(
+    const newEdgesByVertexLabel: Array<Array<Label>> = outboundVertices.map(
         a => [...g.vertices[a].outbound]
             .map(
                 b => [...h.vertices[b].outbound]
